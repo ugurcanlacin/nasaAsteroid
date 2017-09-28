6053ca4a401155a4698c5f846910a9c4c27af0f8
Uğurcan Laçin <ugurcanlacin@gmail.com>
2017-09-28 11:45:11 +0200

    added hazardous function and its test

--- R/nasaAsteroid.R
+++ R/nasaAsteroid.R
@@ -38,11 +38,18 @@ nasaAsteroid <- setRefClass("nasaAsteroid",
                                            estimated_diameter_kilometers_max=numeric(),
                                            is_potentially_hazardous_asteroid=logical(),
                                            close_approach_data_date=as.Date(character()),
-                                           orbital_period=numeric(),stringsAsFactors=FALSE
+                                           orbital_period=numeric(),
+                                           minimum_orbit_intersection=numeric(),
+                                           epoch_osculation=numeric(),
+                                           aphelion_distance=numeric(),
+                                           perihelion_time=numeric(),
+                                           mean_anomaly=numeric(),
+                                           mean_motion=numeric()
+                                           ,stringsAsFactors=FALSE
                           ) 
                           
                           # Fetch 5 pages data from api
-                          for (page in 1:5){
+                          for (page in 1:1){
                             nthPage <- GET("https://api.nasa.gov/neo/rest/v1/neo/browse",
                                               query = list(api_key = api_key,page = page)
                             )
@@ -50,6 +57,7 @@ nasaAsteroid <- setRefClass("nasaAsteroid",
                             # Iterate 20 time for each page to take asteroid information.
                             for(i in 1:20){
                               asteroid <- result$near_earth_objects[[i]]
+                              # print(asteroid)
                               Name <- asteroid$name
                               absolute_magnitude_h <- asteroid$absolute_magnitude_h
                               estimated_diameter_kilometers_min <- asteroid$estimated_diameter$kilometers$estimated_diameter_min
@@ -57,6 +65,13 @@ nasaAsteroid <- setRefClass("nasaAsteroid",
                               is_potentially_hazardous_asteroid <- asteroid$is_potentially_hazardous_asteroid
                               close_approach_data_date <- asteroid$orbital_data$orbit_determination_date
                               orbital_period <- asteroid$orbital_data$orbital_period
+                              
+                              minimum_orbit_intersection <- asteroid$orbital_data$minimum_orbit_intersection
+                              epoch_osculation <- asteroid$orbital_data$epoch_osculation
+                              aphelion_distance <- asteroid$orbital_data$aphelion_distance
+                              perihelion_time <- asteroid$orbital_data$perihelion_time
+                              mean_anomaly <- asteroid$orbital_data$mean_anomaly
+                              mean_motion <- asteroid$orbital_data$mean_motion
                               df <- rbind(df,list(
                                 Name,
                                 absolute_magnitude_h,
@@ -64,7 +79,13 @@ nasaAsteroid <- setRefClass("nasaAsteroid",
                                 estimated_diameter_kilometers_max,
                                 is_potentially_hazardous_asteroid,
                                 close_approach_data_date,
-                                orbital_period
+                                orbital_period,
+                                minimum_orbit_intersection,
+                                epoch_osculation,
+                                aphelion_distance,
+                                perihelion_time,
+                                mean_anomaly,
+                                mean_motion
                               ),stringsAsFactors=FALSE)
                             }
                           }
@@ -74,12 +95,32 @@ nasaAsteroid <- setRefClass("nasaAsteroid",
                                             "estimated_diameter_kilometers_max",
                                             "is_potentially_hazardous_asteroid",
                                             "close_approach_data_date",
-                                            "orbital_period")
+                                            "orbital_period",
+                                            "minimum_orbit_intersection",
+                                            "epoch_osculation",
+                                            "aphelion_distance",
+                                            "perihelion_time",
+                                            "mean_anomaly",
+                                            "mean_motion"
+                                            )
                           near_earth_objects <<- df
-                          print(df)
+                          # print(df)
+                        },
+                        returnDataFrame = function(){
+                          return(near_earth_objects) 
+                        },
+                        hazardousAsteroids = function(){
+                          hazardous <- near_earth_objects[near_earth_objects['is_potentially_hazardous_asteroid'] == TRUE,]
+                          cat("Total asteroid is",nrow(near_earth_objects))
+                          cat("\n")
+                          cat("Total hazardous asteroid number is",nrow(hazardous))
                         }
                       ))
 
 
-# nasa <- nasaAsteroid$new("tYWfgxjr4fPL3KYfmtzWGQvmLcxe7fCciJ3hZjuz")
+nasa <- nasaAsteroid$new("tYWfgxjr4fPL3KYfmtzWGQvmLcxe7fCciJ3hZjuz")
+
+df <- nasa$returnDataFrame()
+
+# nasa$hazardousAsteroids()
 
